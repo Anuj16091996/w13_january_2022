4843b678cebb1c2c8d906742dcf5d092e9320db7
Anuj16091996 <anuj161996@gmail.com>
2022-01-07 16:47:39 -0500

    Assigment Completed LogMsgSync and LogMsg

--- web_server_0/server.js
+++ web_server_0/server.js
@@ -1,13 +1,31 @@
 'use strict'
 
 console.log('NodeJs Interpreter starts executing Javascript code')
+const filesystem = require('fs')
 
-function add (a, b) {
-    return a + b
+function logMsgSync (msg) {
+    if (!filesystem.existsSync('log')) {
+        filesystem.mkdirSync('log')
+        console.log('Directory Created ')
+    }
+    filesystem.appendFileSync('./log/server_log.log', msg + '\n')
 }
 
-const val1 = 2
-const val2 = 6
-const result = add(val1, val2)
-console.log('Hello World')
-console.log('Add function result: ' + result)
+logMsgSync('Create a new File and add this data in that file through syncronize way')
+
+function LogMsg (msg) {
+    filesystem.access('logs', (err) => {
+        if (err == null) {
+            console.log('Log Exits')
+        } else if (err.code === 'ENOENT') {
+            filesystem.mkdir('logs', (err) => {
+                if (err) throw err
+            })
+        }
+        filesystem.appendFile('./logs/server_log.log', msg + '\n', (err) => {
+            if (err) throw err
+        })
+    })
+}
+
+LogMsg('Hello World Through ASync New Message')
