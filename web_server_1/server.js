d75008b344837cdb16aba6caac0c4fd029ac0347
Anuj16091996 <anuj161996@gmail.com>
2022-01-07 23:59:18 -0500

    Called a port 8000 through express app

--- web_server_1/server.js
+++ web_server_1/server.js
@@ -12,4 +12,16 @@
 
 // console.log('Server Listening on port 8000')
 
+let express=require('express')
+let app=express()
+
+app.get('/',
+function(request, respose){
+respose.send('<h1>Hello Worlds</h1>')
+})
+
+app.listen(8000, function(){
+console.log('Port Started at localhost:8000')
+})
+
 
