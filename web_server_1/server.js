33907366b53024a4b66a6c6e36a5b48d17116a69
Anuj16091996 <anuj161996@gmail.com>
2022-01-07 23:52:24 -0500

    Install Express and create eslint for webserver_1

--- web_server_1/server.js
+++ web_server_1/server.js
@@ -1,12 +1,15 @@
  'use strict'
 
-let http=require('http')
+// let http=require('http')
 
-// create a server Object
-http.createServer(function (request, respose){
-    respose.writeHead(200, {'Content-Type':'text-plain'})
-    respose.write('<h1>Hello World</h1>')
-    respose.end()
-} ).listen(8000)
+// // create a server Object
+// http.createServer(function (request, respose){
+//     respose.writeHead(200, {'Content-Type':'text-plain'})
+//     respose.write('<h1>Hello World</h1>')
+//     respose.end()
+// } ).listen(8000)
 
-console.log('Server Listening on port 8000')
+
+// console.log('Server Listening on port 8000')
+
+
