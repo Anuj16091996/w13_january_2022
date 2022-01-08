f98f1a7d47d23ef4f516d0aa4d3c087bf6773ff7
Anuj16091996 <anuj161996@gmail.com>
2022-01-08 15:24:57 -0500

    Create a post method through node sever

--- web_server_1/server.js
+++ web_server_1/server.js
@@ -18,6 +18,33 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+/* POST form processing **********************************************************/
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded())
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json())
+
+// see /public_html/form_post.html
+// display form with http://localhost:8000/form_post.html
+app.post('/form_validate',
+    function (request, response) {
+        // get the form inputs from the body of the HTTP request
+        console.log(request.body)
+        const username = request.body.username
+        const password = request.body.password
+        console.log('username=' + username + ' password=' + password)
+        // process form, validate data …
+        if (username === '' || password === '') {
+            response.writeHead(400, { 'Content-Type': 'text/html' })
+            response.end('missing username or password')
+        } else {
+            response.writeHead(200, { 'Content-Type': 'text/html' })
+            response.end('Thanks for submitting the form')
+        }
+    }
+)
+
 app.use(express.static('public_html'))
 app.get('/',
     function (request, respose) {
@@ -28,6 +55,8 @@ app.get('/byebye',
     function (request, respose) {
         respose.sendFile(path.join(__dirname, 'public_html', 'tables.html'))
     })
+
+// Start Server
 app.listen(8000, function () {
     console.log('Port Started at localhost:8000')
 })
