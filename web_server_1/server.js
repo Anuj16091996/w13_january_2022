624c36e03dac4760f6979d785400cb41927e608f
Anuj16091996 <anuj161996@gmail.com>
2022-01-14 14:31:23 -0500

    Install CORS Framework for acessing domain

--- web_server_1/server.js
+++ web_server_1/server.js
@@ -17,11 +17,11 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-
+const cors = require('cors')
 /* POST form processing **********************************************************/
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded())
-
+app.use(cors())
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json())
 
