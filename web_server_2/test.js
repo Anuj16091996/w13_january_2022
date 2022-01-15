47b42b8bb6ee4c849c05fa32d039a998d4899f2b
Anuj16091996 <anuj161996@gmail.com>
2022-01-15 13:53:31 -0500

    Solve All The assert Error in JSON Format

--- web_server_2/test.js
+++ web_server_2/test.js
@@ -2,48 +2,48 @@
 
 const tableData = require('./src/tableinfile')
 console.log('Debug')
-// const fs = require('fs')
-// // const assert = require('assert')
-// // const JsonData = fs.readFileSync('users.json', 'utf8')
+const fs = require('fs')
+const assert = require('assert')
 
-// // const FinalData = [
-// //     {
-// //         id: 100,
-// //         userName: 'mvachon',
-// //         age: 12
-// //     },
-// //     {
-// //         id: 101,
-// //         userName: 'jcote',
-// //         age: 66
-// //     },
-// //     {
-// //         id: 102,
-// //         userName: 'pmartineau',
-// //         age: 99
-// //     },
-// //     {
-// //         id: 103,
-// //         userName: 'mvachon',
-// //         age: 12
-// //     }
-// // ]
-// assert.deepStrictEqual(JsonData, FinalData)
-
-console.log(tableData.getTable('users.json'))
-console.log(tableData.getRec('users.json', 101))
-// const saveTable = {
-//     title: 'Meeting',
-//     description: 'Meeting John Doe at 10:30 am'
-// }
-//
-const NewEntry =
+const FinalData = [
+    {
+        id: 100,
+        userName: 'mvachon',
+        age: 12
+    },
+    {
+        id: 101,
+        userName: 'jcote',
+        age: 66
+    },
+    {
+        id: 102,
+        userName: 'pmartineau',
+        age: 99
+    },
     {
         id: 104,
         userName: 'Anuj',
         age: 25
     }
+]
+const JsonData = (tableData.getTable('users.json'))
+assert.deepStrictEqual(JsonData, FinalData)
+console.log(JsonData)
+
+// console.log(tableData.getRec('users.json', 101))
+// const saveTable = {
+//     title: 'Meeting',
+//     description: 'Meeting John Doe at 10:30 am'
+// }
+
+// const NewEntry =
+//     {
+//         id: 104,
+//         userName: 'Anuj',
+//         age: 25
+//     }
 
 // tableData.addRec('users.json', NewEntry)
-tableData.saveTable('users.json', NewEntry)
-tableData.updateRec('users.json', NewEntry)
+// tableData.saveTable('users.json', NewEntry)
+// tableData.updateRec('users.json', NewEntry)
