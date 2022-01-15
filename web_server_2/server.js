e1cd2f2eee918d025822188a5573da9142f4813e
Anuj16091996 <anuj161996@gmail.com>
2022-01-15 13:26:38 -0500

    Created web_server_2: Exercise fs, assert modules and JSON: manipulating JSON data stored in a file

--- web_server_2/server.js
+++ web_server_2/server.js
@@ -4,3 +4,17 @@ const tableData = require('./src/tableinfile')
 console.log('Debug')
 
 console.log(tableData.getTable('users.json'))
+console.log(tableData.getRec('users.json', 101))
+// const saveTable = {
+//     title: 'Meeting',
+//     description: 'Meeting John Doe at 10:30 am'
+// }
+// tableData.saveTable('users.json', saveTable)
+// const NewEntry =
+//     {
+//         id: 104,
+//         userName: 'mvachon',
+//         age: 12
+//     }
+
+// tableData.addRec('users.json', NewEntry)
--- web_server_2/src/tableinfile.js
+++ web_server_2/src/tableinfile.js
@@ -11,19 +11,46 @@ function getTable (fileName) {
 }
 
 function getRec (fileName, id) {
-
+    const tableData = fs.readFileSync(fileName, 'utf8')
+    const getTableJSon = JSON.parse(tableData)
+    for (let i = 0; i < getTableJSon.length; i++) {
+        if (getTableJSon[i].id === id) { return getTableJSon[i] }
+        if (i === getTableJSon.length - 1) { throw new Error('id not found') }
+    }
 }
-
 function saveTable (fileName, datas) {
-
+    const datatoString = JSON.stringify(datas)
+    fs.writeFile(fileName, datatoString)
 }
 
 function addRec (fileName, data) {
+    const tableData = fs.readFileSync(fileName, 'utf8')
+    const getTableJSon = JSON.parse(tableData)
 
+    for (let i = 0; i < getTableJSon.length; i++) {
+        if (getTableJSon[i].id === data.id) { throw new Error('id already exists') }
+        if (i === getTableJSon.length - 1) {
+            getTableJSon.push(data)
+            const jsonDataString = JSON.stringify(getTableJSon)
+            // console.log(jsonDataString)
+            fs.writeFileSync(fileName, jsonDataString)
+        }
+    }
 }
-
 function updateRec (fileName, data) {
-
+    let idExists = false
+    const data = fs.readFileSync(fileName)
+    const jsonDataArray = JSON.parse(data)
+    for (let i = 0; i < jsonDataArray.length; i++) {
+    if (newData.id === jsonDataArray[i].id) {
+            jsonDataArray[i] = newData
+            idExists = true
+        }
+    }
+    if (idExists) {
+        const jsonDataString = JSON.stringify(jsonDataArray)
+        fs.writeFileSync(fileName, jsonDataString)
+    } else {        throw 'id not found'}
 }
 
 module.exports = {
