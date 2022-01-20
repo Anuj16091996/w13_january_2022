1bfcca19f151a5d6720a6c359dbc52dd1253758d
Anuj16091996 <anuj161996@gmail.com>
2022-01-20 13:23:34 -0500

    Remove some console.log and extra spaces

--- web_server_3/server.js
+++ web_server_3/server.js
@@ -3,11 +3,10 @@ const express = require('express')
 const app = express()
 
 const cors = require('cors')
-/* POST form processing **********************************************************/
-// Parse URL-encoded bodies (as sent by HTML forms)
+
 app.use(express.urlencoded())
 app.use(cors())
-// Parse JSON bodies (as sent by API clients)
+
 app.use(express.json())
 app.use(express.static('public_html'))
 
