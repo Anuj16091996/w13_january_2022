c0d022d71045764bbdf0a0fe81b6f71ba00c8e05
Anuj16091996 <anuj161996@gmail.com>
2022-01-19 23:08:39 -0500

    Completed The whole excerise and change the interface

--- web_server_3/server.js
+++ web_server_3/server.js
@@ -22,6 +22,7 @@ app.get('/offices', function (request, response) {
         response.writeHead(200, { 'Content-Type': 'application/json' })
         // send out a string
         response.end(officeJSONString)
+        DB.disconnect()
     })
 })
 
@@ -34,12 +35,13 @@ app.get('/offices/:id', function (request, response) {
     DB.connect()
     DB.queryParams('SELECT * from offices where officecode=$1', [numberUserID], function (offices) {
         const officeJSON = { offices: offices.rows }
-        const officeJSONString = JSON.stringify(officeJSON, null, 4)
+        const officeJSONString = JSON.stringify(officeJSON)
 
         // set content type
         response.writeHead(200, { 'Content-Type': 'application/json' })
         // send out a string
         response.end(officeJSONString)
+        DB.disconnect()
     })
 })
 
@@ -56,19 +58,20 @@ app.post('/offices', function (request, response) {
                 response.writeHead(403, { 'Content-Type': 'application/json' })
                 // send out a string
                 response.end('Data Already exists')
+                DB.disconnect()
             } else {
                 response.writeHead(200, { 'Content-Type': 'application/json' })
                 // send out a string
                 response.end('Data Added')
+                DB.disconnect()
             }
         })
 })
 
 app.put('/offices/:id', function (request, response) {
     const userID = request.params.id
-
     const numberUserID = Number(userID)
-    console.log(typeof numberUserID)
+
     const userDetail = request.body
     console.log(userDetail)
     const DB = require('./src/dao')
@@ -79,12 +82,14 @@ app.put('/offices/:id', function (request, response) {
             response.writeHead(403, { 'Content-Type': 'application/json' })
             // send out a string
             response.end('Data did not exists')
+            DB.disconnect()
         } else {
             DB.queryParams('update offices set city=$2, phone=$3, addressline1=$4, addressline2=$5, state=$6 ,country=$7, postalcode=$8, territory=$9 where officecode=$1', [numberUserID, userDetail.city, userDetail.phone, userDetail.addressline1, userDetail.addressline2, userDetail.state, userDetail.country, userDetail.postalcode, userDetail.territory],
                 function (resp) {
                     response.writeHead(200, { 'Content-Type': 'application/json' })
                     // send out a string
                     response.end('Data Updated')
+                    DB.disconnect()
                 })
         }
     })
@@ -103,6 +108,7 @@ app.delete('/offices/:id', function (request, response) {
                 response.writeHead(403, { 'Content-Type': 'application/json' })
                 // send out a string
                 response.end('Data did not exists')
+                DB.disconnect()
             } else {
                 DB.queryParams('delete  from offices where officecode=$1', [numberUserID],
                     function (resp) {
@@ -115,6 +121,7 @@ app.delete('/offices/:id', function (request, response) {
                             response.writeHead(200, { 'Content-Type': 'application/json' })
                             // send out a string
                             response.end('Data deleted')
+                            DB.disconnect()
                         }
                     })
             }
