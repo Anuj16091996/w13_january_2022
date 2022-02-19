8b8164c112648f8a7ad942903a8e80dfe20eca69
Anuj16091996 <anuj161996@gmail.com>
2022-02-19 11:52:04 -0500

    Create a backend server JS with Node Js and frond end with react

---
+++ final-project-client/src/playlist.js
@@ -0,0 +1,291 @@
+import React from 'react'
+
+
+
+
+class Playlist extends React.Component {
+    constructor(props) {
+        super(props)
+          // set initial state
+        // do not use setState in constructor, write state directly
+        this.state = {
+        music_albums : [], // will contain dresses data array from server
+        music_index : 0, // the index of the dress currently shown, start at first in array
+        music_count : 0, // how many dresses in data array from server
+        isLoaded : false,  // will be true after data have been received from server
+        error : null ,      // no errors yet !
+        server_messgae:'',
+        discog_albums:[],
+        playlist_album:[],
+        dropdownValue:0
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+
+    handleChange(e) {
+
+        this.setState({ dropdownValue: e.target.value });
+      }
+
+
+    componentDidMount() {
+        fetch('http://localhost:8000/track')
+        .then(
+            (response)=> {
+
+                if (response.ok) {
+                    response.json().then(json_response =>{
+                        console.log(json_response.track)
+                        this.setState({
+                            music_albums:json_response.track,
+                            music_count: Object.keys(json_response.track).length,
+                            music_index : 0,
+                            isLoaded : true,
+                            error : null
+                        })
+                    })
+                }else{
+                    response.json().then(json_response => {
+                        this.setState({
+                            isLoaded: false,
+
+                        })
+                    })
+                }
+            }
+        )
+
+
+
+    }
+
+    DeleteData=(id)=>{
+        //this.setState({[event.target.name]:event.target.value})
+
+        let url='http://localhost:8000/track/'+ id
+
+        fetch(url,
+            {
+                method: 'delete',
+            }
+        )
+        .then((response)=>{
+            if (response.ok) {
+                this.setState({server_messgae:'Data Deleted'})
+                this.componentDidMount()
+            }else{
+                this.setState({server_messgae:'Data can not be deleted'})
+            }
+        })
+
+    }
+
+    AddDataToPlayList=(id)=>{
+
+        this.setState( {'':this.state.discog_albums.splice(id,1) })
+
+        let playlist_id=this.state.dropdownValue
+
+
+        let title=this.state.discog_albums[id].title
+
+        let uri='http://www.discogs.com'+this.state.discog_albums[id].uri
+
+        let master_id=this.state.discog_albums[id].master_id
+
+        let playlist={
+            title:title,
+            playlist_id:playlist_id,
+            uri:uri,
+            master_id:master_id
+        }
+
+        console.log(playlist)
+        fetch('http://localhost:8000/track',
+            {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                    //'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: JSON.stringify(playlist)
+            }
+        )
+
+            .then((response) => {
+                if (response.ok) {
+                    this.setState({server_messgae:'Data Inserted'})
+                    this.componentDidMount()
+                } else {
+                    this.setState({server_messgae:'Data Error'})
+                }
+            })
+    }
+
+    SearchDataThroughAPI=()=>{
+        let key='ZyZwXQsAyfGdBUukMtkc'
+        let secret='ltXEwZDWSKFvbcZwFxqldAcSSyjSlDel'
+        let search=document.getElementById('Artist_Name').value
+        document.getElementById('Artist_Name').value=''
+
+        let url='https://api.discogs.com/database/search?key='+key+'&secret='+secret+'&artist='+search+'&country=canada'
+        console.log(url)
+        fetch(url,
+            {
+                method: 'get',
+            }
+        )
+        .then((response)=>{
+
+            if (response.ok) {
+
+                response.json().then(json_response =>{
+
+                        if(Object.keys(json_response.results).length===0){
+                            this.setState({server_messgae:'Not Found'})
+                        }else{
+                            this.setState({
+                                discog_albums:json_response.results
+
+                            })
+                            console.log(json_response.results)
+                        }
+
+                })
+            }else{
+                this.setState({server_messgae:'API error'})
+            }
+        })
+
+        fetch('http://localhost:8000/playlist')
+        .then(
+            (response)=> {
+
+                if (response.ok) {
+                    response.json().then(json_response =>{
+
+                        this.setState({
+                            playlist_album:json_response.track
+                        })
+                    })
+                }
+            }
+        )
+
+
+    }
+
+        render() {
+            let dataBaseArrayItems=this.state.music_albums.map((data,index) => {
+                return(
+
+                    <tbody key={index}>
+                        <td >
+                    {data.title}  <br/>
+                    ID: {data.id}<br/>
+                    Playlist :{data.playlist_title}<br/>
+                    <a href={data.uri}> {data.uri} </a>
+                    </td>
+
+
+                    <td>
+                        <button onClick={()=>this.DeleteData(data.id)} >Delete </button>
+                    </td>
+
+
+                    </tbody>
+
+                )
+            })
+
+            let SelectItems=this.state.playlist_album.map((data,index)=>{
+                return(
+
+                    <option key={index} value={data.id}> {data.title}</option>
+                    )
+            })
+
+            let discordArrayItems=this.state.discog_albums.map((data,index) => {
+                return(
+                    <tbody key={index}>
+                    <td >
+                <b>{data.title}</b>  <br/>
+                <img src={data.cover_image} alt="Cover_Image" height='200px' width='200px' /><br/>
+
+                Style :{data.style}<br/>
+                Format:{data.format}<br/>
+                ID: {data.id}<br/>
+                {data.country}-{data.year }<br/>
+                Master ID-{data.master_id}<br/>
+                <a href={data.uri}> {data.uri} </a><br/>
+
+                <a href={'http://www.discogs.com'+ data.uri} > More Information </a>
+                </td>
+
+                <td>
+                    Select your genre<br/>
+                    <select value={this.state.dropdownValue} onChange={this.handleChange} >
+                        {SelectItems}
+                    </select>
+                      </td>
+
+                <td>
+                    <button onClick={()=>this.AddDataToPlayList(index)} > Add </button>
+                </td>
+
+
+                </tbody>
+                )
+
+            })
+            return(
+
+                <div>
+                    <p>Server Message: { this.state.server_messgae}</p>
+               <div className='DisplayContent'>
+
+               <div className='Playlist'>
+               <h4>My Playlist</h4>
+               <div className='' >
+                   <table>
+
+                       {dataBaseArrayItems}
+
+                   </table>
+
+
+               </div>
+               </div>
+
+               <div className='API'>
+               <h4>Music Provided By Discogs.com  </h4>
+               <pre><b>Search By Artist</b> <input type="text" id='Artist_Name' /><b> Candian releases only </b></pre>
+
+               <button onClick={()=>this.SearchDataThroughAPI()} >Search </button><br/>
+
+               <div>
+
+
+                {discordArrayItems}
+
+
+
+
+               </div>
+               </div>
+
+
+               </div>
+               </div>
+            )
+        }
+
+
+
+}
+
+
+
+
+export default Playlist
---
+++ final-project-server/server.js
@@ -0,0 +1,154 @@
+
+'use strict'
+
+const fetch = require('cross-fetch')
+const express = require('express')
+const secure = require('https')
+const app = express()
+
+// Use CORS to allow Ajax requests from other web sites
+// const cors = require('cors')
+// app.use(cors())
+const fs = require('fs')
+const { response } = require('express')
+const privateKey = fs.readFileSync('privatekey.pem', 'utf8')
+const certificate = fs.readFileSync('publiccert.pem', 'utf8')
+
+const credentials = {
+    key: privateKey,
+    cert: certificate
+
+}
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded())
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json())
+
+// path module is built-in, part of basic node, no need to install
+// const path = require('path')
+
+// serve static html/css/js files, images etc..
+// good old web site files
+// in folder public_html
+app.use(express.static('public_html'))
+
+//* * ROUTES ************************************************************/
+
+// Show all tracks
+app.get('/track', (req, res) => {
+    const DB = require('./src/dao')
+    DB.connect()
+    DB.query('select t.*, p.title as playlist_title FROM track t inner join playlist p on p.id = t.playlist_id order by id asc', (track) => {
+        if (track.rowCount > 0) {
+            const trackJSON = { msg: 'All tracks', track: track.rows } // keep only the data records rows
+            const trackJSONString = JSON.stringify(trackJSON, null, 4) // convert JSON to JSON data string
+            res.writeHead(200, { 'Content-Type': 'application/json' }) // set HTTP response content type for JSON
+            res.end(trackJSONString) // send out the JSON data string
+        } else {
+            // set content type
+            const tracksJSON = { msg: 'Table empty, no tracks found' }
+            const trackJSONString = JSON.stringify(tracksJSON, null, 4)
+            res.writeHead(404, { 'Content-Type': 'application/json' })
+            // send out a string
+            res.end(trackJSONString)
+        }
+        DB.disconnect()
+    })
+})
+const axios = require('axios').default
+
+// Show All Playlist
+app.get('/playlist', (req, res) => {
+    const DB = require('./src/dao')
+    DB.connect()
+    DB.query('select * from playlist order by id asc', (playlist) => {
+        if (playlist.rowCount > 0) {
+            const trackJSON = { msg: 'All tracks', track: playlist.rows } // keep only the data records rows
+            const trackJSONString = JSON.stringify(trackJSON, null, 4) // convert JSON to JSON data string
+            res.writeHead(200, { 'Content-Type': 'application/json' }) // set HTTP response content type for JSON
+            res.end(trackJSONString) // send out the JSON data string
+        } else {
+            // set content type
+            const tracksJSON = { msg: 'Table empty, no tracks found' }
+            const trackJSONString = JSON.stringify(tracksJSON, null, 4)
+        }
+        DB.disconnect()
+    })
+})
+app.get('/trackies', (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://restcountries.com/v3.1/all/'
+
+    }
+    axios.request(options).then(function (response) {
+        const trackJSONString = JSON.stringify(response.data, null, 4)
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        // send out a string
+        res.end(trackJSONString)
+    })
+}
+)
+
+// Add a new track
+app.post('/track',
+    function (request, response) {
+        // get the form inputs from the body of the HTTP request
+        console.log(request.body)
+        const playlistId = Number(request.body.playlist_id)
+        const title = request.body.title
+        const uri = request.body.uri
+        const masterId = request.body.master_id
+        const DB = require('./src/dao')
+        DB.connect()
+
+        DB.queryParams('INSERT INTO track (playlist_id,title,uri,master_id) VALUES ($1,$2,$3,$4)',
+            [playlistId, title, uri, masterId], function (tracks) {
+                const trackJSON = { msg: 'OK track added' }
+                const trackJSONString = JSON.stringify(trackJSON, null, 4)
+                // set content type
+                response.writeHead(200, { 'Content-Type': 'application/json' })
+                // send out a string
+                response.end(trackJSONString)
+                DB.disconnect()
+            })
+    }
+)
+
+app.get('/track',
+    function (request, response) {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/plain')
+        // get the form inputs from the body of the HTTP request
+        response.end('Hello World')
+    }
+)
+
+// DELETE
+app.delete('/track/:id', function (request, response) {
+    const id = request.params.id // read the :id value send in the URL
+    const DB = require('./src/dao')
+    DB.connect()
+
+    DB.queryParams('DELETE from track WHERE id=$1', [id], function (tracks) {
+        const trackJSON = { msg: 'OK track deleted' }
+        const trackJSONString = JSON.stringify(trackJSON, null, 4)
+        // set content type
+        response.writeHead(200, { 'Content-Type': 'application/json' })
+        // send out a string
+        response.end(trackJSONString)
+        DB.disconnect()
+    })
+})
+const https = secure.createServer(credentials, app)
+
+https.listen(8000,
+    function (err) {
+        if (err) {
+            console.log('in')
+        }
+        console.log('Node server listening on port 8000')
+    }
+)
